08fd4a520952accfa2140098131eea1d6bf3d5f7
sicaoo <65303575+sicaoo@users.noreply.github.com>
2020-05-15 15:29:32 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,6 +16,8 @@
 </head>
 <body>
 
+
+
 <div id="main">
     <div id="header">
         <div id="logo">
@@ -28,7 +31,7 @@
             <ul id="menu">
                 <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
                 <li><a href="index.php">Kezdőoldal</a></li>
-                <li><a href="akciok.php">Akciók</a></li>
+                <li><a href="akciok.html">Akciók</a></li>
                 <li><a href="news.php">Újdonságok</a></li>
                 <li><a href="order.php">Megrendelés</a></li>
                 <li><a href="contact.php">Kontakt</a></li>
@@ -46,28 +49,202 @@
             </form>
             <h1>Kategóriák</h1>
             <ul>
-                <li><a href="viragmagok.php">Virágmagok</a></li>
-                <li><a href="#">Vetőmagok</a></li>
-                <li><a href="#">Kaktuszok</a></li>
-                <li><a href="#">Fák</a></li>
+                <li><a href="#viragmagok">Virágmagok</a></li>
+                <li><a href="#vetomagok">Vetőmagok</a></li>
+                <li><a href="#kaktuszok">Kaktuszok</a></li>
+                <li><a href="#fak">Fák</a></li>
             </ul>
 
 
 
         </div>
         <div id="content">
-            <h1>Welcome to the night_sky template</h1>
-            <p>This standards compliant, simple, fixed width website template is released as an 'open source' design (under a <a href="http://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 Licence</a>), which means that you are free to download and use it for anything you want (including modifying and amending it). All I ask is that you leave the 'design from HTML5webtemplates.co.uk' link in the footer of the template, but other than that...</p>
-            <p>This template is written entirely in <strong>HTML5</strong> and <strong>CSS</strong>, and can be validated using the links in the footer.</p>
-            <p>You can view more free HTML5 web templates <a href="http://www.html5webtemplates.co.uk">here</a>.</p>
-            <p>This template is a fully functional 5 page website, with an <a href="examples.html">examples</a> page that gives examples of all the styles available with this design.</p>
-            <h2>Browser Compatibility</h2>
-            <p>This template has been tested in the following browsers:</p>
-            <ul>
-                <li>Internet Explorer 8</li>
-                <li>FireFox 3</li>
-                <li>Google Chrome 13</li>
-            </ul>
+		<style>
+		.a{
+		border:1px solid #808080;
+		padding:10px;
+		float:center;
+		text-align:center;
+		line-height:150%;
+		
+
+		}
+		.b{
+			background-color:#90ee90;
+			border:1px solid #808080;
+			margin 5px;
+		}
+		.b img {
+		
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+		width: 50%;
+		}
+		img:hover {
+		box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+		}
+
+		</style>
+        <?php
+		 header('Content-Type: text/html; charset=utf-8');
+
+	
+	echo"<div id=\"viragmagok\">";
+$connect=mysql_connect("localhost","root","") or die ("Couldn't connect");
+$query89=mysql_query("SET NAMES 'UTF8'");
+$query98=mysql_query("SET CHARACTER SET UTF8");
+mysql_select_db("eden") or die ("Couldn't find db");
+
+
+$id = array();
+$resultid = mysql_query("SELECT product_id FROM product");
+while ($row = mysql_fetch_assoc($resultid)) {
+   $id = array_merge($id, array_map('trim', explode(",", $row['product_id']))); //ID to array
+}
+$name = array();
+$resultname = mysql_query("SELECT product_name FROM product");
+while ($row = mysql_fetch_assoc($resultname)) {
+   $name= array_merge($name, array_map('trim', explode(",", $row['product_name']))); // name to array
+}
+$description = array();
+$resultdescription = mysql_query("SELECT description FROM product");
+while ($row = mysql_fetch_assoc($resultdescription)) {
+   $description= array_merge($description, array_map('trim', explode(",", $row['description']))); //descriptionto array
+}
+$price = array();
+$resultprice = mysql_query("SELECT price FROM product");
+while ($row = mysql_fetch_assoc($resultprice)) {
+   $price= array_merge($price, array_map('trim', explode(",", $row['price']))); //price to array
+}
+$discount = array();
+$resultdiscount = mysql_query("SELECT discount FROM product");
+while ($row = mysql_fetch_assoc($resultdiscount)) {
+   $discount= array_merge($discount, array_map('trim', explode(",", $row['discount']))); //discount to array
+}
+$product_type = array();
+$resultpt = mysql_query("SELECT product_type FROM product");
+while ($row = mysql_fetch_assoc($resultpt)) {
+   $product_type= array_merge($product_type, array_map('trim', explode(",", $row['product_type']))); // product type to array
+}
+
+
+
+		
+$j=count($id);
+for ($i=0; $i<$j ; $i++)
+{	
+	
+	
+		if ($product_type[$i]=='virágmag'){
+		echo "<div class=\"b\">";
+		$db = mysqli_connect("localhost","root","","eden"); 
+		$sql = "SELECT id,file FROM images WHERE id=$i";
+		$sth = $db->query($sql);
+		$image=array();
+		$result=mysqli_fetch_array($sth);
+		echo '<img src="data:image/jpeg;base64,'.base64_encode( $result['file'] ).'" />';
+		echo "</div>";
+		
+		echo "<div class=\"a\">";
+		echo "A termék kódja:".$id[$i]. "<br> ";
+		echo "Neve:".$name[$i] . "<br>";
+		echo "Leírás:".$description[$i]. "<br>";
+		echo "Ára:".$price[$i]. "rsd  <br>";
+		echo "Raktáron:".$discount[$i]. "<br>";
+		echo "<br>";
+		echo "</div>";
+				
+	}
+}
+echo "</div>"; 
+
+echo"<div id=\"vetomagok\">";
+for ($i=0; $i<$j ; $i++)
+{	
+	
+	
+		if ($product_type[$i]=='vetőmag'){
+		echo "<div class=\"b\">";
+		$db = mysqli_connect("localhost","root","","eden"); 
+		$sql = "SELECT id,file FROM images WHERE id=$i";
+		$sth = $db->query($sql);
+		$image=array();
+		$result=mysqli_fetch_array($sth);
+		echo '<img src="data:image/jpeg;base64,'.base64_encode( $result['file'] ).'" />';
+		echo "</div>";
+		
+		echo "<div class=\"a\">";
+		echo "A termék kódja:".$id[$i]. "<br> ";
+		echo "Neve:".$name[$i] . "<br>";
+		echo "Leírás:".$description[$i]. "<br>";
+		echo "Ára:".$price[$i]. "rsd  <br>";
+		echo "Raktáron:".$discount[$i]. "<br>";
+		echo "<br>";
+		echo "</div>";
+				
+	}
+}
+echo "</div>"; 
+
+echo"<div id=\"kaktuszok\">";
+for ($i=0; $i<$j ; $i++)
+{	
+	
+	
+		if ($product_type[$i]=='kaktusz'){
+		echo "<div class=\"b\">";
+		$db = mysqli_connect("localhost","root","","eden"); 
+		$sql = "SELECT id,file FROM images WHERE id=$i";
+		$sth = $db->query($sql);
+		$image=array();
+		$result=mysqli_fetch_array($sth);
+		echo '<img src="data:image/jpeg;base64,'.base64_encode( $result['file'] ).'" />';
+		echo "</div>";
+		
+		echo "<div class=\"a\">";
+		echo "A termék kódja:".$id[$i]. "<br> ";
+		echo "Neve:".$name[$i] . "<br>";
+		echo "Leírás:".$description[$i]. "<br>";
+		echo "Ára:".$price[$i]. "rsd  <br>";
+		echo "Raktáron:".$discount[$i]. "<br>";
+		echo "<br>";
+		echo "</div>";
+				
+	}
+}
+echo "</div>"; 
+	echo"<div id=\"fak\">";	
+	for ($i=0; $i<$j ; $i++)
+{	
+	
+	
+		if ($product_type[$i]=='fa'){
+		echo "<div class=\"b\">";
+		$db = mysqli_connect("localhost","root","","eden"); 
+		$sql = "SELECT id,file FROM images WHERE id=$i";
+		$sth = $db->query($sql);
+		$image=array();
+		$result=mysqli_fetch_array($sth);
+		echo '<img src="data:image/jpeg;base64,'.base64_encode( $result['file'] ).'" />';
+		echo "</div>";
+		
+		echo "<div class=\"a\">";
+		echo "A termék kódja:".$id[$i]. "<br> ";
+		echo "Neve:".$name[$i] . "<br>";
+		echo "Leírás:".$description[$i]. "<br>";
+		echo "Ára:".$price[$i]. "rsd  <br>";
+		echo "Raktáron:".$discount[$i]. "<br>";
+		echo "<br>";
+		echo "</div>";
+				
+	}
+}
+echo "</div>";
+?>
+
+
+		</div>
         </div>
     </div>
     <div id="footer">
