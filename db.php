28a05accc7b2f0c82bfef0dd528ace57fd8ccb77
99reni <65304652+99reni@users.noreply.github.com>
2020-05-19 01:57:29 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,45 +1,59 @@
 <?php
-require_once "db_config.php";
+header('Content-Type: text/html; charset=utf-8');
 
 
-$connect = new mysqli(HOST,USER,PASS,DATABASE);
-/*if($connect -> connect_errno) {
-    echo $connect -> connect__error;
+
+$connect=mysql_connect("localhost","root","") or die ("Couldn't connect");
+$query89=mysql_query("SET NAMES 'UTF8'");
+$query98=mysql_query("SET CHARACTER SET UTF8");
+mysql_select_db("eden") or die ("Couldn't find db");
+
+
+$id = array();
+$resultid = mysql_query("SELECT product_id FROM product");
+while ($row = mysql_fetch_assoc($resultid)) {
+    $id = array_merge($id, array_map('trim', explode(",", $row['product_id']))); //ID to array
 }
-// Get data from database
-$sql = "SELECT product_id, product_name,product_type, description, price, image  FROM product";
-if ($result = $connect -> query($sql)) {
-    while ($row = $result -> fetch_assoc()) {
-        $products[] = $row;
-    }
+$name = array();
+$resultname = mysql_query("SELECT product_name FROM product");
+while ($row = mysql_fetch_assoc($resultname)) {
+    $name= array_merge($name, array_map('trim', explode(",", $row['product_name']))); // name to array
 }
-$sql_str = "";
-if (!empty($products)) {
-    foreach ($products as $key => $value) {
-        echo '<div class="product-list__item">
-                                <img src="'.$value['image'].'" class="product-list__picture" alt="">
-                                <div class="product-list__title">'.$value['product_name'].'</div>
-                                <div class="product-list__description">'.$value['description'].'</div>
-                                <div class="product-list__price">'.$value['price'].' din.</div>
-                            </div>';
-    }
+$description = array();
+$resultdescription = mysql_query("SELECT description FROM product");
+while ($row = mysql_fetch_assoc($resultdescription)) {
+    $description= array_merge($description, array_map('trim', explode(";", $row['description']))); //descriptionto array
 }
-else {
-    echo '<p class="products-error"> Products not found </p>';
-}*/
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$price = array();
+$resultprice = mysql_query("SELECT price FROM product");
+while ($row = mysql_fetch_assoc($resultprice)) {
+    $price= array_merge($price, array_map('trim', explode(",", $row['price']))); //price to array
 }
+$discount = array();
+$resultdiscount = mysql_query("SELECT discount FROM product");
+while ($row = mysql_fetch_assoc($resultdiscount)) {
+    $discount= array_merge($discount, array_map('trim', explode(",", $row['discount']))); //discount to array
+}
+$product_type = array();
+$resultpt = mysql_query("SELECT product_type FROM product");
+while ($row = mysql_fetch_assoc($resultpt)) {
+    $product_type= array_merge($product_type, array_map('trim', explode(",", $row['product_type']))); // product type to array
+}
+
+//insert to database
+if(isset($_POST['submit'])){
+    $sql = "INSERT INTO member (first_name, last_name,address,email,phone)
+        VALUES ('".$_POST["first_name"]."','".$_POST["last_name"]."','".$_POST["address"]."','".$_POST["email"]."','".$_POST["telephone"]."')
+        FROM eden";
+
+        alert("Sikeresen hozzáadva");
+}
+
+
+
+
+
 ?>
-
-?>
-
-
-
-
 
 
 
