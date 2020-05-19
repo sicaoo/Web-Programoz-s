7027295b68d8e6236c1b7f147a2fdc400ef5ed94
99reni <65304652+99reni@users.noreply.github.com>
2020-05-19 02:05:52 +0200

    Update news.php

--- news.php
+++ news.php
@@ -45,35 +45,19 @@
             </form>
             <h1>Kategóriák</h1>
             <ul>
-                <li><a href="#">Virágmagok</a></li>
-                <li><a href="#">Vetőmagok</a></li>
-                <li><a href="#">Kaktuszok</a></li>
-                <li><a href="#">Fák</a></li>
+                <li><a href="viragmagok.php">Virágmagok</a></li>
+                <li><a href="vetomagok.php">Vetőmagok</a></li>
+                <li><a href="kaktuszok.php">Kaktuszok</a></li>
+                <li><a href="fak.php">Fák</a></li>
             </ul>
-            <h1>Latest News</h1>
-            <h4>New Website Launched</h4>
-            <h5>January 1st, 2010</h5>
-            <p>2010 sees the redesign of our website. Take a look around and let us know what you think.<br /><a href="#">Read more</a></p>
+
 
 
         </div>
-        <div id="content">
-            <h1>Welcome to the night_sky template</h1>
-            <p>This standards compliant, simple, fixed width website template is released as an 'open source' design (under a <a href="http://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 Licence</a>), which means that you are free to download and use it for anything you want (including modifying and amending it). All I ask is that you leave the 'design from HTML5webtemplates.co.uk' link in the footer of the template, but other than that...</p>
-            <p>This template is written entirely in <strong>HTML5</strong> and <strong>CSS</strong>, and can be validated using the links in the footer.</p>
-            <p>You can view more free HTML5 web templates <a href="http://www.html5webtemplates.co.uk">here</a>.</p>
-            <p>This template is a fully functional 5 page website, with an <a href="examples.html">examples</a> page that gives examples of all the styles available with this design.</p>
-            <h2>Browser Compatibility</h2>
-            <p>This template has been tested in the following browsers:</p>
-            <ul>
-                <li>Internet Explorer 8</li>
-                <li>FireFox 3</li>
-                <li>Google Chrome 13</li>
-            </ul>
-        </div>
+
     </div>
     <div id="footer">
-        <p><a href="index.php">Kezdőoldal</a> | <a href="akciok.html">Akciók</a> | <a href="news.php">Újdonságok</a> | <a href="order.php">Megrendelés</a> | <a href="contact.php">Kontakt</a></p>
+        <p><a href="index.php">Kezdőoldal</a> | <a href="akciok.php">Akciók</a> | <a href="news.php">Újdonságok</a> | <a href="order.php">Megrendelés</a> | <a href="contact.php">Kontakt</a></p>
         <p>Copyright &copy; éden </p>
     </div>
 </div>
