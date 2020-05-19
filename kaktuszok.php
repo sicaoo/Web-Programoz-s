6f6fad4b3192e484fa01a15d35c39e7045cceac0
99reni <65304652+99reni@users.noreply.github.com>
2020-05-19 02:11:54 +0200

    Update kaktuszok.php

--- kaktuszok.php
+++ kaktuszok.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width initial-scale=1">
     <link rel="stylesheet" type="text/css" href="style/style.css" />
+    <link rel="stylesheet" type="text/css" href="style/style3.css" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
